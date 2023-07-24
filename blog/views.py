61c14af8213d68954ee5ad315ee8e65cab1432f3
Hanminho <hihi6024@gmail.com>
2023-07-24 20:59:32 +0900

    페이지네이션 및 잘못된 페이지로 이동 시 오류 처리

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    post_list = Post.published.all()
+    # 페이지 당 3개의 게시물로 페이지네이션
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page')
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # page_number가 정수가 아닌 경우 첫번째 페이지 제공
+        posts = paginator.page(1)
+    except EmptyPage:
+        # page_number가 범위를 벗어난 경우 결과의 마지막 페이지 제공
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',
+                  {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
