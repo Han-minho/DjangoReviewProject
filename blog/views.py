36015db15db0fabd304f2162e3ab0fe8d7a15560
Hanminho <hihi6024@gmail.com>
2023-07-24 21:37:16 +0900

    클래스 기반 뷰 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -35,3 +35,10 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListview(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
