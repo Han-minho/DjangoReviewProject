9fb2f5ae3e34b9dcc7b303606366e7643a62d15d
Hanminho <hihi6024@gmail.com>
2023-07-24 19:05:15 +0900

    카탈로그 고급기능

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset()\
+        return super().get_queryset() \
             .filter(status=Post.Status.PUBLISHED)
 
 
@@ -17,7 +18,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish', allow_unicode=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -32,8 +33,8 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT
     )
-    objects = models.Manager() # 기본 매니저
-    published = PublishedManager() # 사용자 정의 매니저
+    objects = models.Manager()  # 기본 매니저
+    published = PublishedManager()  # 사용자 정의 매니저
 
     class Meta:
         ordering = ['-publish']
@@ -43,3 +44,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
-    #re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$',
-     #       views.post_detail, name='post_detail'),
+    # path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+
 ]
