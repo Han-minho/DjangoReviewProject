e410021bbfcdf546f439dc8cc2356800baa43512
Hanminho <hihi6024@gmail.com>
2023-07-25 15:33:17 +0900

    카탈로그 태그 기능 생성

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    path('list/', views.post_list, name='post_list'),
     path('', views.PostListview.as_view(), name='post_list'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$',
             views.post_detail, name='post_detail'),
@@ -13,4 +13,5 @@ urlpatterns = [
     # path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
 ]
