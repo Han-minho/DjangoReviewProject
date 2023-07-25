13dd6ddf20f397fc1d72adb233bbc874e8c5ce34
Hanminho <hihi6024@gmail.com>
2023-07-25 15:50:10 +0900

    게시물 검색 기능 개발

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from blog.forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
@@ -33,25 +34,27 @@ def post_list(request, tag_slug=None):
 
 
 def post_detail(request, year, month, day, post):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("No Post found.")
     post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
+                             status=Post.Status.PUBLISHED, 
                              slug=post,
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    # List of active comments for this post
-    comments = post.comments.filter(active=True)
+    # List of active comments for this post 
+    comments = post.comments.filter(active=True) 
     # Form for users to comment
     form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True) 
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 class PostListview(ListView):
