b2fc14aa597b6fe66aceb686b28494d7b77ee2ff
Hanminho <hihi6024@gmail.com>
2023-07-25 19:34:02 +0900

    트라이그램 유사도를 사용한 검색

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post, Comment
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
-from blog.forms import EmailPostForm, CommentForm
+from blog.forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import TrigramSimilarity
+from django.contrib.postgres.search import SearchVector
 
 
 # Create your views here.
@@ -32,20 +34,20 @@ def post_list(request, tag_slug=None):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED, 
+                             status=Post.Status.PUBLISHED,
                              slug=post,
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
     # List of active comments for this post 
-    comments = post.comments.filter(active=True) 
+    comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
     # List of similar posts
-    post_tags_ids = post.tags.values_list('id', flat=True) 
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
         .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
@@ -99,3 +101,21 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
