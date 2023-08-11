be11d2aca532ae0d870870705a79ce5c0699ac45
Hanminho <hihi6024@gmail.com>
2023-08-11 10:56:52 +0900

    장바구니 삭제 뷰 로직

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from decimal import Decimal
 from shop.models import Product
 
 
@@ -18,4 +18,32 @@ def add_cart(request, product_id):
 
 
 def cart_detail(request):
-    return render(request, 'cart/detail.html', {})
+    cart = request.session.get('cart', {})
+    product_ids = cart.keys()
+    products = Product.objects.filter(id__in=product_ids)
+    for product in products:
+        cart[str(product.id)]['product'] = product
+    for key, item in cart.items():
+        item['price'] = Decimal(item['price'])
+        item['total_price'] = item['price'] * item['quantity']
+        cart[key] = item
+
+    return render(request, 'cart/detail.html', {
+        'cart_dict': cart
+    })
+
+
+def cart_remove(request, product_id):
+    cart = request.session.get('cart', {})
+    del cart[str(product_id)]
+    request.session['cart'] = cart
+    request.session.method = True
+    return redirect('shop:cart_detail')
+
+
+def cart_update(request, product_id):
+    cart = request.session.get('cart', {})
+    cart[str(product_id)]['quantity'] = Decimal(request.POST.get('quantity'))
+    request.session['cart'] = cart
+    request.session.modified = True
+    return redirect('shop:cart_detail')
